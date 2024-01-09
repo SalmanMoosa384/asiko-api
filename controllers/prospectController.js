d5f84147440e25fc1c03b83f975f97bcfbd4695c
SalmanMoosa384 <salmanmoosa36@gmail.com>
2024-01-09 00:42:15 -0800

    map to loop

--- controllers/prospectController.js
+++ controllers/prospectController.js
@@ -9,53 +9,59 @@ const prospectController = async function (reqBody) {
     );
     let limit = reqBody?.limit ? reqBody.limit : 1;
     let count = 0;
-    let responseDetail = {};
-    responseDetail.profiles = [];
+    let responseDetail = { profiles: [] };
     let companyDetail = await getProfile(reqBody.companylinkedinURL, "company");
 
-    if (companyDetail.data?.profile_type) {
+    if (companyDetail?.data?.profile_type) {
       responseDetail.companyDetail = companyDetail.data;
 
-      let response = await Promise.all(
-        reqBody.jobTitles.map(async (jobtitle) => {
-          let companyId = companyDetail.data.details.company_id;
+      for (const jobtitle of reqBody.jobTitles) {
+        if (count >= limit) {
+          break;
+        }
 
-          let peopleSearchData = await peopleSearch(
-            companyId,
+        let companyId = companyDetail.data.details.company_id;
+
+        let peopleSearchData = await peopleSearch(
+          companyId,
+          jobtitle,
+          helpers.iScrapperLimit(limit)
+        );
+        peopleSearchData.data.results = helpers.sortByTitle(
+          peopleSearchData.data.results,
+          jobtitle
+        );
+
+        for (const profile of peopleSearchData.data.results) {
+          if (count >= limit) {
+            break;
+          }
+
+          let profileDetail = await getProfile(profile.profile_id, "personal");
+          console.log(
+            profile.profile_id,
+            count,
+            limit,
             jobtitle,
-            helpers.iScrapperLimit(limit)
+            count < limit
           );
-          peopleSearchData.data.results = helpers.sortByTitle(
-            peopleSearchData.data.results,
-            jobtitle
-          );
-          for (const profile of peopleSearchData.data.results) {
-            if (count == limit) {
-              break;
-            }
-            else{
-            let profileDetail = await getProfile(
-              profile.profile_id,
-              "personal"
-            );
-            let getCurrentCompanyPosition =
-              profileDetail.data.position_groups.filter((prof) => {
-                return (
-                  prof.company.id == companyId &&
-                  prof.profile_positions[0].title.toLocaleLowerCase().trim() ==
-                    jobtitle.toLocaleLowerCase().trim()
-                );
-              });
-            if (getCurrentCompanyPosition.length > 0) {
-              count = count + 1;
-              responseDetail.profiles.push(profileDetail.data);
-            } else {
-              break;
-            }
+
+          let getCurrentCompanyPosition =
+            profileDetail.data.position_groups.filter((prof) => {
+              return (
+                prof.company.id == companyId &&
+                prof.profile_positions[0].title.toLocaleLowerCase().trim() ==
+                  jobtitle.toLocaleLowerCase().trim()
+              );
+            });
+          if (getCurrentCompanyPosition.length > 0) {
+            count = count + 1;
+            responseDetail.profiles.push(profileDetail.data);
+          } else {
+            break;
           }
-          }
-        })
-      );
+        }
+      }
     } else {
       return { success: false, data: "company not found" };
     }
